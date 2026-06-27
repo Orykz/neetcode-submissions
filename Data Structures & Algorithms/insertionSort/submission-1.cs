e07bf5e2666e997b736592a1f5d225e3f24b6fac
Jero Pardo <jero.a.pardo@gmail.com>
2026-06-27 23:02:45 +0800

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.cs
@@ -0,0 +1,28 @@
+// Definition for a pair
+// public class Pair {
+//     public int Key;
+//     public string Value;
+//
+//     public Pair(int key, string value) {
+//         Key = key;
+//         Value = value;
+//     }
+// }
+public class Solution {
+    public List<List<Pair>> InsertionSort(List<Pair> pairs) {
+        List<List<Pair>> pairList = [];
+
+        for (int i = 0; i < pairs.Count; i++) {
+            int j = i - 1;
+            while (j >= 0 && pairs[j + 1].Key < pairs[j].Key) {
+                var temp = pairs[j + 1];
+                pairs[j + 1] = pairs[j];
+                pairs[j] = temp;
+                j--;
+            }
+            var newList = new List<Pair>(pairs);
+            pairList.Add(newList);
+        }
+        return pairList;
+    }
+}
