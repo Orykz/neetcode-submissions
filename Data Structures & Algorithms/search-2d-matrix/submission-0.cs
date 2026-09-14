7627b7bd2331b5cb44e708f5d3db55a593603924
Jero Pardo <191698697+Orykz@users.noreply.github.com>
2026-09-15 03:51:16 +0800

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int ROWS = matrix.Length;
+        int COLS = matrix[0].Length;
+
+        int top = 0, bot = ROWS - 1;
+        int row = 0;
+
+        while (top <= bot)
+        {
+            row = (top + bot) / 2;
+            if (target > matrix[row][COLS - 1])
+            {
+                top = row + 1;
+            } else if (target < matrix[row][0])
+            {
+                bot = row - 1;
+            } else
+            {
+                break;
+            }
+        }
+        
+        if (top > bot) return false;
+
+        int left = 0, right = COLS - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (matrix[row][mid] > target)
+            {
+                right = mid - 1;
+            } else if (matrix[row][mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
